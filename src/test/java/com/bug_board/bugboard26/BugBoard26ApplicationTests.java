023a91667ecfa23244af869eb9fc18e64230eddd
Tulliola <fulviocoppola28@gmail.com>
2025-11-22 17:50:31 +0100

    Commit per aggiustare repository git

---
+++ src/test/java/com/bug_board/bugboard26/BugBoard26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bug_board.bugboard26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBoard26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
