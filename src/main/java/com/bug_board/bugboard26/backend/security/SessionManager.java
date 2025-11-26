bf3f8e33bde760d7328161f769a089bb2c46ba1e
Tulliola <fulviocoppola28@gmail.com>
2025-11-26 12:45:59 +0100

    Aggiunta classe SessionManager per memorizzare il JWT restituito dall'autenticazione. Bisogna gestire l'associazione del token nell'istanza di SessionManager

---
+++ src/main/java/com/bug_board/bugboard26/backend/security/SessionManager.java
@@ -0,0 +1,39 @@
+package com.bug_board.bugboard26.backend.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public final class SessionManager {
+
+    private static SessionManager instance;
+
+    private volatile String jwtToken;
+
+    private SessionManager() {
+    }
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            synchronized (SessionManager.class) {
+                if (instance == null)
+                    instance = new SessionManager();
+            }
+        }
+
+        return instance;
+    }
+
+    public void createSession(String token) {
+        this.jwtToken = token;
+    }
+
+    public void clearSession() {
+        this.jwtToken = null;
+    }
+
+    public boolean isLoggedIn() {
+        return jwtToken != null;
+    }
+}
