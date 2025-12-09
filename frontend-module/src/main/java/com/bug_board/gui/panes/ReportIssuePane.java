1839684bea5b678af780111b861c6adc10e2f4b4
justantxnio <contean0013@gmail.com>
2025-12-09 23:11:53 +0100

    aggiunti al form di report di un'issue i campi per inserire tipo di issue, titolo, descrizione e priorità (da terminare)

--- frontend-module/src/main/java/com/bug_board/gui/panes/ReportIssuePane.java
+++ frontend-module/src/main/java/com/bug_board/gui/panes/ReportIssuePane.java
@@ -1,18 +1,26 @@
 package com.bug_board.gui.panes;
 
+import com.bug_board.enum_classes.IssueTipology;
 import com.bug_board.presentation_controllers.HomePagePC;
+import com.bug_board.utilities.animations.FloatingLabel;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.io.ByteArrayInputStream;
+import java.util.function.UnaryOperator;
 
 public class ReportIssuePane extends StackPane {
     private StackPane parentContainer;
@@ -30,26 +38,34 @@ public class ReportIssuePane extends StackPane {
     private RadioButton documentationRadioButton = new RadioButton("Documentation");
     private RadioButton newFeatureRadioButton = new RadioButton("New Feature");
 
+    private HBox fillFormTextBox = new HBox();
+    private Text fillFormText = new Text("Fill out the following form to submit your report");
+
+    private HBox issueAttributesBox = new HBox();
+    private VBox titleAndDescriptionBox = new VBox();
+    private TextField titleTextField= new TextField();
+    private TextArea descriptionTextArea = new TextArea();
+    private VBox priorityBox = new VBox();
+    private ComboBox<String> priorityComboBox = new ComboBox<>();
+
+    private static final int MAX_TITLE_CHARS = 50;
+    private static final int MAX_DESCRIPTION_CHARS = 300;
+
     public ReportIssuePane(StackPane parentContainer, HomePagePC homePagePC) {
         this.parentContainer = parentContainer;
         this.homePagePC = homePagePC;
-
         this.initialize();
     }
 
 
     private void initialize() {
-        this.setBackground();
         this.setStyle();
+        this.setBackground();
         this.getChildren().add(setContentPane());
     }
 
     private VBox setContentPane() {
-        typeIssueText.setStyle("-fx-font-size: 20px");
-        typeIssueText.setTextAlignment(TextAlignment.CENTER);
-        typeIssueBox.getChildren().add(typeIssueText);
-        typeIssueBox.setAlignment(Pos.CENTER);
-        contentPane.getChildren().add(typeIssueBox);
+        contentPane.getChildren().add(setTypeIssueBox());
 
 
         bugRadioButton.setToggleGroup(typeIssueToggleGroup);
@@ -57,23 +73,145 @@ public class ReportIssuePane extends StackPane {
         documentationRadioButton.setToggleGroup(typeIssueToggleGroup);
         newFeatureRadioButton.setToggleGroup(typeIssueToggleGroup);
 
-        typeIssueRadioButtonBox.getChildren().add(bugRadioButton);
-        typeIssueRadioButtonBox.getChildren().add(questionRadioButton);
-        typeIssueRadioButtonBox.getChildren().add(documentationRadioButton);
-        typeIssueRadioButtonBox.getChildren().add(newFeatureRadioButton);
+        typeIssueRadioButtonBox.getChildren().add(setRadioButton(
+                bugRadioButton,
+                new Image(new ByteArrayInputStream(IssueTipology.Bug.getAssociatedImage())),
+                "Report malfunctioning"));
+        typeIssueRadioButtonBox.getChildren().add(setRadioButton(
+                questionRadioButton,
+                new Image(new ByteArrayInputStream(IssueTipology.Question.getAssociatedImage())),
+                "Request for clarity"
+        ));
+        typeIssueRadioButtonBox.getChildren().add(setRadioButton(
+                documentationRadioButton,
+                new Image(new ByteArrayInputStream(IssueTipology.Documentation.getAssociatedImage())),
+                "Report issues regarding documentation"
+        ));
+        typeIssueRadioButtonBox.getChildren().add(setRadioButton(
+                newFeatureRadioButton,
+                new Image(new ByteArrayInputStream(IssueTipology.NewFeature.getAssociatedImage())),
+                "Request or suggest new feature"
+        ));
 
+        contentPane.getChildren().add(setIssueRadioButtonBox());
 
+        contentPane.getChildren().add(setFillFormBox());
+
+        contentPane.getChildren().add(setAttributesBox());
+
+        return contentPane;
+    }
+
+    private Node setAttributesBox() {
+        issueAttributesBox.setAlignment(Pos.CENTER);
+        issueAttributesBox.getChildren().add(setTitleAndDescriptionBox());
+        issueAttributesBox.getChildren().add(setPriorityBox());
+
+        HBox.setHgrow(titleAndDescriptionBox, Priority.ALWAYS);
+        HBox.setHgrow(priorityBox, Priority.ALWAYS);
+
+        return issueAttributesBox;
+    }
+
+    private Node setPriorityBox() {
+        priorityBox.setAlignment(Pos.TOP_RIGHT);
+        String highPriority = new String("High");
+        String mediumPriority = new String("Medium");
+        String lowPriority = new String("Low");
+        String noPriority = new String("Don't specify");
+
+        priorityComboBox.setItems(FXCollections.observableArrayList(
+                highPriority,
+                mediumPriority,
+                lowPriority,
+                noPriority
+        ));
+
+        priorityComboBox.setMinWidth(300);
+        priorityComboBox.setMaxWidth(300);
+        priorityComboBox.getSelectionModel().select("Don't specify");
+
+        priorityBox.getChildren().add(priorityComboBox);
+
+        priorityBox.setPadding(new Insets(0, 0, 0, 50));
+        return priorityBox;
+    }
+
+    private Node setTitleAndDescriptionBox() {
+        UnaryOperator<TextFormatter.Change> titleFilter = change -> {
+            String newText = change.getControlNewText();
+
+            if (newText.length() > MAX_TITLE_CHARS)
+                return null;
+            return change;
+        };
+
+        titleTextField.setTextFormatter(new TextFormatter<>(titleFilter));
+
+        descriptionTextArea.setWrapText(true);
+        descriptionTextArea.getStyleClass().add("no-border-textarea");
+        UnaryOperator<TextFormatter.Change> descriptionFilter = change -> {
+            String newText = change.getControlNewText();
+
+            if (newText.length() > MAX_DESCRIPTION_CHARS)
+                return null;
+            return change;
+        };
+
+        descriptionTextArea.setTextFormatter(new TextFormatter<>(descriptionFilter));
+
+        StackPane textFieldWrapper = FloatingLabel.createFloatingLabelField(titleTextField, "Title (max "+MAX_TITLE_CHARS+"  characters)");
+        VBox.setMargin(textFieldWrapper, new Insets(0, 0, 20, 0));
+
+        StackPane textAreaWrapper = FloatingLabel.createFloatingLabelField(descriptionTextArea, "Title (max "+MAX_DESCRIPTION_CHARS+" characters)");
+        VBox.setMargin(textAreaWrapper, new Insets(20, 0, 20, 0));
+
+        titleAndDescriptionBox.getChildren().addAll(textFieldWrapper, textAreaWrapper);
+        return titleAndDescriptionBox;
+    }
+
+    private Node setFillFormBox() {
+        fillFormTextBox.setAlignment(Pos.CENTER);
+        fillFormText.setStyle("-fx-font-size: 24px");
+        fillFormTextBox.getChildren().add(fillFormText);
+        fillFormTextBox.setPadding(new Insets(20));
+
+        return fillFormTextBox;
+    }
+
+    private Node setTypeIssueBox() {
+        typeIssueText.setStyle("-fx-font-size: 24px");
+        typeIssueText.setTextAlignment(TextAlignment.CENTER);
+        typeIssueBox.getChildren().add(typeIssueText);
+        typeIssueBox.setAlignment(Pos.CENTER);
+
+        return typeIssueBox;
+    }
+
+    private Node setIssueRadioButtonBox(){
         typeIssueRadioButtonBox.setAlignment(Pos.CENTER);
         typeIssueRadioButtonBox.setSpacing(15);
-        typeIssueRadioButtonBox.setPadding(new Insets(20, 20, 20, 20));
+        typeIssueRadioButtonBox.setPadding(new Insets(20));
+        typeIssueRadioButtonBox.setStyle("-fx-border-color: white white -color-primary white");
 
-        contentPane.getChildren().add(typeIssueRadioButtonBox);
-        return contentPane;
+        return typeIssueRadioButtonBox;
+    }
+
+    public RadioButton setRadioButton(RadioButton radioButtonToSet, Image radioButtonImage, String tooltipString){
+        ImageView imageView = new ImageView(radioButtonImage);
+        imageView.setFitWidth(50);
+        imageView.setFitHeight(50);
+        imageView.setTranslateX(-4);
+        Tooltip tooltip = new Tooltip(tooltipString);
+        tooltip.setShowDelay(Duration.millis(100));
+        tooltip.install(radioButtonToSet, tooltip);
+        radioButtonToSet.setGraphic(imageView);
+
+        return radioButtonToSet;
     }
 
     private void setBackground() {
         this.setStyle("-fx-background-color: rgb(0, 0, 0, 0.6);");
-
         this.setOnMouseClicked(event -> {
             if(event.getTarget() == this)
                 close();
@@ -81,8 +219,8 @@ public class ReportIssuePane extends StackPane {
     }
 
     private void setStyle() {
-        this.getStyleClass().add("overlay-pane");
-        this.setStyle("-fx-max-height: 750px; -fx-min-height: 750px; -fx-min-width: 1000px; -fx-max-width: 1000px");
+        contentPane.getStyleClass().add("overlay-pane");
+        contentPane.setStyle("-fx-max-height: 750px; -fx-min-height: 750px; -fx-min-width: 1000px; -fx-max-width: 1000px");
     }
 
     public void close() {
