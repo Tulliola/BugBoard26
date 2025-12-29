71b2628e58b61c1b646f1c0b05c651bcaac4e261
justantxnio <contean0013@gmail.com>
2025-12-29 17:03:01 +0100

    Iniziati test validi black box

--- backend-module/src/test/java/com/bug_board/backendmodule/services/implementations/jpa/IssueServiceJPAUnitTest.java
+++ backend-module/src/test/java/com/bug_board/backendmodule/services/implementations/jpa/IssueServiceJPAUnitTest.java
@@ -4,16 +4,14 @@ import com.bug_board.backendmodule.repositories.interfaces.IIssueRepository;
 import com.bug_board.backendmodule.services.implementations.jpa_implementations.IssueServiceJPA;
 import com.bug_board.backendmodule.services.interfaces.ILabelService;
 import com.bug_board.backendmodule.services.interfaces.IProjectService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @ExtendWith(MockitoExtension.class)
-public class IssueServiceJPATest {
+public class IssueServiceJPAUnitTest {
     @Mock
     private IIssueRepository issueRepository;
 
@@ -26,4 +24,8 @@ public class IssueServiceJPATest {
     @InjectMocks
     private IssueServiceJPA issueServiceJPA;
 
+    @Test
+    public void testValidPublishOfAnIssue(){
+        
+    }
 }
